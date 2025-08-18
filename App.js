2d57e2c580dd7e1c999bbe7a1bf62a7041bcc51f
alok <alokjha2504@gmail.com>
2025-08-18 18:29:02 +0530

    Project setup with npm, parcel and React

---
+++ App.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+
+const AppLayout = () => {
+    return (
+        <div>
+         Hello
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>)
